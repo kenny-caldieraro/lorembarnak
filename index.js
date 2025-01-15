c6375e17e34860d3432183b6cfec9dc76fa1fab1
kenny <kenny@nematis.com>
2025-01-15 09:36:13 +0100

    debug

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const express = require('express');
 const app = express();
 const { getText } = require('lorembarnak');
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/lorem', (req, res) => {
-    console.log('req.body', req.body);
-    const custom = req.body.text;
-    console.log('custom', custom);
     let quote;
-    // if (custom.length > 0) {
-    // quote = getText(parseInt(custom));
-    // } else {
-    quote = getText();
-    // }
+    if (custom && !isNaN(custom)) {
+        quote = getText(parseInt(custom));
+    } else {
+        quote = getText();
+    }
 
     res.json({
         response_type: 'in_channel',
