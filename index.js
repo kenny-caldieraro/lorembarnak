df8d1c4a857da35eb8ff94bac80249415463cfd7
kenny <kenny@nematis.com>
2025-01-15 09:33:30 +0100

    debug

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const { getText } = require('lorembarnak');
 app.use(express.json());
 
 app.post('/lorem', (req, res) => {
-    console.log(req.body);
-    // const custom = req.body.text;
+    console.log('req.body', req.body);
+    const custom = req.body.text;
+    console.log('custom', custom);
     let quote;
     // if (custom.length > 0) {
     // quote = getText(parseInt(custom));
